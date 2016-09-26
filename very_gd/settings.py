5060e80a584f2b1d22d79908462007f6dc594c9d
Andrew <andreas@alphanumeric02.domain>
2016-09-26 12:21:46 -0700

    Add CORS support.

--- very_gd/settings.py
+++ very_gd/settings.py
@@ -24,6 +24,8 @@ DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CORS_ORIGIN_ALLOW_ALL = True
+
 MEDIA_PORTAL_SETTINGS = {
     'REQUEST_SETUP_VIEW': 'very_gd.views.RequestSetup',
     'CONTENT_SERIALIZER': 'panel.serializers.PanelSerializer'
@@ -95,6 +97,7 @@ INSTALLED_APPS += (
 
 MIDDLEWARE_CLASSES = (
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
