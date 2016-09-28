e2661861272fd8a2ac477f81f76bfb872560a053
0taste <irlboyy@gmail.com>
2016-09-28 00:24:16 -0700

    fixed issue #41

--- web/components/header/header.js
+++ web/components/header/header.js
@@ -8,7 +8,23 @@ angular.module('ua5App')
                 basic: '@'
             },
             link: function($scope, element, attrs) {
-            }
+            },
+            controller: ['$scope', '$state', '$rootScope', function($scope, $state, $rootScope) {
+                $scope.backButtonToggle = false;
+                $scope.goBack = function() {
+                    if ($rootScope.previousState !== undefined) {
+
+                        $state.go($rootScope.previousState.name);
+                    }
+                    
+                };
+                $rootScope.$on('$stateChangeSuccess', function(event, to, toParams, from, fromParams) {
+                    //save the previous state in a rootScope variable so that it's accessible from everywhere
+                    $rootScope.previousState = from;
+                    $scope.backButtonToggle = (to.name === 'projects.details') ? true : false;
+                    
+                });
+            }]
         };
     }])
 ;
