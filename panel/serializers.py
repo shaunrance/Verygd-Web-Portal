db162c50b06039c607c7e6a735a62489dc53ed83
Andrew <andreas@alphanumeric02.domain>
2016-09-30 12:07:51 -0700

    Make 'order' optional.

--- panel/serializers.py
+++ panel/serializers.py
@@ -5,7 +5,7 @@ from media_portal.album.content.serializers.base import ContentSerializer
 class PanelSerializer(ContentSerializer):
     tag = serializers.CharField()
     related_tag = serializers.CharField(required=False)
-    order = serializers.IntegerField()
+    order = serializers.IntegerField(required=False)
 
     def create(self, validated_data):
         instance = super(PanelSerializer, self).create(validated_data)
