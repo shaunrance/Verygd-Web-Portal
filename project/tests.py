a0d6e061c7abf0328adf0230395fa1277774be2a
Andrew <andreas@alphanumeric02.domain>
2016-10-05 16:17:29 -0700

    Allow 'related_tag' to be nullable or blank.

--- project/tests.py
+++ project/tests.py
@@ -46,6 +46,31 @@ class TestProject(TestImageAPI):
 
         self.assertTrue('related_tag' in image_meta and image_meta['related_tag'] == 'cool')
 
+    def test_can_make_related_tag_blank(self):
+        response, image_meta = self.add_image(
+            self.member,
+            self.album_id,
+            tag='tag',
+            related_tag='related_tag',
+            order=5,
+            is_panorama=True
+        )
+
+        self.assertEquals(response.status_code, 201, 'got {0} expected 201'.format(response.status_code))
+
+        response, msg = self.put_as(self.member,
+                                    '/images/{0}'.format(image_meta['id']),
+                                    data={'related_tag': None},
+                                    content_type='application/json')
+
+        self.assertEquals(response.status_code, 200)
+
+        response, image_meta = self.get_as(self.member, '/images/{0}'.format(msg['id']))
+
+        self.assertEquals(response.status_code, 200, 'got {0} expected 201'.format(response.status_code))
+
+        self.assertTrue('related_tag' in image_meta and image_meta['related_tag'] is None)
+
     def test_update_project(self):
         response, msg = self.put_as(self.member, '/album/{0}'.format(self.album_id), data={
             'title': 'new title', 'background_color': 'salmon'
