b2fe80873392e719ec028285a23ae62ee1a6980b
Greg Carr <gregcarrart@gmail.com>
2016-10-13 14:01:31 -0700

    preview widget - add header toggle switch UI

---
+++ web/components/toggle-switch/toggle-switch.js
@@ -0,0 +1,45 @@
+/* global angular */
+angular.module('ua5App')
+    .directive('toggleSwitch', function() {
+        return {
+            restrict: 'EA',
+            replace: true,
+            require: 'ngModel',
+            scope: {
+                disabled: '@',
+                color: '@'
+            },
+            templateUrl:'components/toggle-switch/toggle-switch.html',
+            compile: function(element, attrs) {
+                return this.link;
+            },
+            link: function(scope, element, attrs, ngModelCtrl) {
+                element.on('click', function() {
+                    scope.$apply(scope.toggle);
+                });
+
+                ngModelCtrl.$formatters.push(function(modelValue) {
+                    return modelValue;
+                });
+
+                ngModelCtrl.$parsers.push(function(viewValue) {
+                    return viewValue;
+                });
+
+                ngModelCtrl.$viewChangeListeners.push(function() {
+                    scope.$eval(attrs.ngChange);
+                });
+
+                ngModelCtrl.$render = function() {
+                    scope.model = ngModelCtrl.$viewValue;
+                };
+
+                scope.toggle = function toggle() {
+                    if (!scope.disabled) {
+                        scope.model = !scope.model;
+                        ngModelCtrl.$setViewValue(scope.model);
+                    }
+                };
+            }
+        };
+    });
