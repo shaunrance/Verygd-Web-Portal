a7ea6431037ecde6209656ec5c95ff2519aa22af
Andrew <andreas@alphanumeric02.domain>
2016-10-13 22:43:41 -0700

    Add setup commands to fabric file.  #91.

--- fabfile.py
+++ fabfile.py
@@ -4,9 +4,9 @@ from fabric.operations import run
 
 from server_setup import initial_install as init
 
-
-working_dir = '/var/local/very.gd'
-remove_venv = '/'.join([working_dir, '../verygd_venv'])
+env_dir = env.roles[0]
+working_dir = '/var/local/very.gd/{0}'.format(env_dir)
+remote_venv = '/'.join([working_dir, '../../verygd_venv', env_dir])
 
 env.roledefs = {
     'staging': ['ec2-52-53-186-20.us-west-1.compute.amazonaws.com']
@@ -18,7 +18,7 @@ def update_files():
 
 
 def install_upgrade_reqs():
-    run('source {venv_dir}/bin/activate && pip install -r requirements.txt --upgrade'.format(venv_dir=remove_venv))
+    run('source {venv_dir}/bin/activate && pip install -r requirements.txt --upgrade'.format(venv_dir=remote_venv))
 
 
 def restart_uwsgi():
@@ -26,7 +26,7 @@ def restart_uwsgi():
 
 
 def run_migrations():
-    run('source {venv_dir}/bin/activate && ./manage.py migrate'.format(venv_dir=remove_venv))
+    run('source {venv_dir}/bin/activate && ./manage.py migrate'.format(venv_dir=remote_venv))
 
 
 def setup():
@@ -37,7 +37,7 @@ def setup():
 
 
 def initial_install():
-    return init(venv_dir=remove_venv, working_dir=working_dir)
+    return init(venv_dir=remote_venv, working_dir=working_dir)
 
 
 def deploy():
--- server_setup.py
+++ server_setup.py
@@ -9,12 +9,12 @@ sys_files_dir = 'system'
 
 
 def checkout_repo(in_dir):
-    print('cloning repo..')
+    print('cloning repo..\n')
     sudo('git clone git@github.com:verygd/Verygd-Web-Portal.git {0}'.format(in_dir))
 
 
 def setup_working_dir(working_dir_name):
-    print('creating working directory ({0})'.format(working_dir_name))
+    print('creating working directory ({0})\n'.format(working_dir_name))
 
     sudo('mkdir -p {0}'.format(working_dir_name))
 
@@ -24,6 +24,8 @@ def setup_working_dir(working_dir_name):
 
 def copy(repo_conf, system_file):
     if not has_file(system_file):
+        print('{repo_conf} -> {system_file}'.format(repo_conf=repo_conf, system_file=system_file))
+
         sudo('cp {repo_conf} {system_file}'.format(repo_conf=repo_conf, system_file=system_file))
 
 
@@ -37,55 +39,76 @@ def create_tmp_socket(named):
     sudo('chmod 600 ' + socket)
 
 
-def setup_supervisord(working_dir):
-    print('setting up supervisord..')
+def setup_supervisord(*args, **kwargs):
+    print('setting up supervisord..\n')
 
     sudo('pip install supervisor')
 
-    copy(repo_config_path(working_dir, 'supervisord.conf'), '/etc/supervisord.conf')
+    create_tmp_socket('supervisor.sock')
+
+    copy(repo_config_path(kwargs['working_dir'], 'supervisord.conf'), '/etc/supervisord.conf')
 
 
 def run_supervisord():
+    if has_file('/tmp/supervisord.pid'):
+        sudo('kill -HUP `cat /tmp/supervisord.pid`')
     sudo('supervisord -c /etc/supervisord.conf')
 
 
-def setup_nginx(working_dir):
-    print('setting up nginx..')
+def setup_nginx(*args, **kwargs):
+    print('setting up nginx..\n')
+
+    nginx_log_dir = '/var/local/nginx/log/'
 
     sudo('apt-get install nginx')
 
-    conf_dir = '/etc/nginx/sites-available/very.gd/{0}/'.format(env_dir)
+    if not has_file(nginx_log_dir):
+        sudo('mkdir -p {0}'.format(nginx_log_dir))
+
+    conf_dir = '/etc/nginx/sites-available/very.gd/{0}'.format(env_dir)
 
     sudo('mkdir -p {0}'.format(conf_dir))
 
-    copy(repo_config_path(working_dir, 'nginx.conf'), conf_dir)
+    copy(repo_config_path(kwargs['working_dir'], 'nginx.conf'), '/'.join([conf_dir, 'nginx.conf']))
+
+
+def setup_uwsgi_params(filename='/var/local/uwsgi/uwsgi_params', *args, **kwargs):
+    if not has_file(filename):
+        copy(repo_config_path(kwargs['working_dir'], 'uwsgi_params'), filename)
 
 
 def setup_uwsgi(*args, **kwargs):
-    print('setting up uwsgi..')
+    print('setting up uwsgi..\n')
+
+    setup_uwsgi_params(**kwargs)
 
     conf_dir = '/etc/uwsgi/sites/python3/very.gd/{0}'.format(env_dir)
+    log_dir = '/var/local/uwsgi/log/'
 
-    sudo('source {venv_dir}/bin/activate && pip install uwsgi'.format(venv_dir=kwargs['venv_dir']))
+    sudo('TMPDIR=/tmp pip install uwsgi'.format(venv_dir=kwargs['venv_dir']))
 
-    create_tmp_socket('very_gd_{0}_uwsgi'.format(env_dir))
+    create_tmp_socket('very_gd_{0}_uwsgi.sock'.format(env_dir))
 
     if not has_file(conf_dir):
         sudo('mkdir -p {0}'.format(conf_dir))
+
+    if not has_file(log_dir):
+        sudo('mkdir -p {0}'.format(log_dir))
 
     # this is needed for python3
     copy(repo_config_path(kwargs['working_dir'], 'spawn_python3.ini'), '/'.join([conf_dir, 'uwsgi.ini']))
 
     # copy main uwsgi file
-    copy(repo_config_path(kwargs['working_dir'], '{0}/uwsgi.ini'.format(env_dir)), '/'.join([conf_dir, 'uwsgi.ini']))
+    copy(repo_config_path(kwargs['working_dir'], '{0}/uwsgi.ini'.format(env_dir)),
+         '/'.join(['/etc/uwsgi/sites', 'uwsgi.ini']))
 
 
-def setup_virtualenv(venv_dir):
-    print('setting up virtualenv..')
+def setup_virtualenv(*args, **kwargs):
+    print('setting up virtualenv..\n')
 
-    sudo('mkdir -p {0}'.format(venv_dir))
+    sudo('mkdir -p {0}'.format(kwargs['venv_dir']))
     sudo('pip install --upgrade virtualenv')
-    sudo('virtualenv -p python3 {0}/{1}'.format(venv_dir, env_dir))
+    sudo('virtualenv -p python3 {0}'.format(kwargs['venv_dir']))
 
 
 def has_file(named):
@@ -117,9 +140,10 @@ def has_virtualenv(venv_dir):
 
 
 def initial_install(*args, **kwargs):
-    print('checking for existing setup..')
+    print('checking for existing setup..\n')
 
     sudo('apt-get update')
+    sudo('apt-get install python3-dev')
 
     if not has_file(kwargs['working_dir']):
         setup_working_dir(**kwargs)
@@ -127,13 +151,10 @@ def initial_install(*args, **kwargs):
     if not has_virtualenv(kwargs['venv_dir']):
         setup_virtualenv(**kwargs)
 
-    if not has_uwsgi():
-        setup_uwsgi(**kwargs)
+    setup_uwsgi(**kwargs)
 
-    if not has_nginx():
-        setup_nginx(**kwargs)
+    setup_nginx(**kwargs)
 
-    if not has_supervisord():
-        setup_supervisord(**kwargs)
+    setup_supervisord(**kwargs)
 
-        run_supervisord()
+    run_supervisord()
