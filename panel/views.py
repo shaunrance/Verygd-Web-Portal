8321ff1bf90e0df4986f9be9b5cf8dcb77bd24ed
Andrew <andreas@alphanumeric02.domain>
2016-10-19 16:31:46 -0700

    Set queryset for viewset.

--- panel/views.py
+++ panel/views.py
@@ -3,3 +3,9 @@ from panel.models import PanelImage
 
 
 PanelImageViewSet.model = PanelImage
+
+
+def get_queryset(self):
+    request_member = self.request.user.member
+
+    return self.model.objects.filter()
