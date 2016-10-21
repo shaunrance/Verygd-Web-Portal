9865c0f852a1aea445727710c5933613265becc2
Christopher Black <Huckpilot@Christophers-MacBook-Pro.local>
2016-10-20 18:47:23 -0700

    Add billing and payent plan.

--- web/modals/billingModal.js
+++ web/modals/billingModal.js
@@ -1,6 +1,11 @@
 /* global angular */
 angular.module('ua5App')
-	.controller('billingModalController', ['$scope', '$rootScope', '$state', '$element', 'fields', 'close', 'UsersResource', 'AuthResource', '$cookies', 'APICONSTANTS', function($scope, $rootScope, $state, $element, fields, close, UsersResource, AuthResource, $cookies, APICONSTANTS) {
+	.controller('billingModalController', ['$timeout', '$scope', '$rootScope', '$state', '$element', 'fields', 'close', 'UsersResource', 'AuthResource', '$cookies', 'APICONSTANTS', function($timeout, $scope, $rootScope, $state, $element, fields, close, UsersResource, AuthResource, $cookies, APICONSTANTS) {
+        var userId = $cookies.get(APICONSTANTS.authCookie.user_id);
+        $scope.annualChecked = true;
+        $scope.monthlyChecked = false;
+        $scope.premiumClicked = false;
+
         $scope.title = 'Professional Plan';
         $scope.currentPlan = '$25.00/mo';
         $scope.formLabels = 'dfdsf';
@@ -12,54 +17,82 @@ angular.module('ua5App')
                 description: ''
             }
         };
+
         $scope.close = function() {
             close({
                 input: $scope.input.fields
             });
         };
+
         $scope.cancel = function() {
             close({
                 input: $scope.input.fields
             });
         };
 
-        $scope.updateUser = function(data) {
-            var user;
+        $scope.showPremium = function() {
+            $scope.premiumClicked = true;
+        };
 
-            user = {
-                name: data.name,
-                email: data.email,
-                password: data.password,
-                payment: {
-                    plan_name: data.paymentType,
-                    card: {
-                        name: data.cardName,
-                        number: data.cardNumber,
-                        exp_month: data.month,
-                        exp_year: data.year,
-                        cvc: data.cvc,
-                        address_zip: data.zip
-                    }
-                }
-            };
-
-            UsersResource.user().update(user).$promise.then(
+        function initialValues() {
+            UsersResource.user().retrieve({id: userId}).$promise.then(
                 function(response) {
-                    AuthResource.token().retrieve({username: user.email, password: user.password}).$promise.then(
-                        function(response) {
-                            //set cookie token && then go to projects
-                            $cookies.put(APICONSTANTS.authCookie.token, response.token);
-                            $cookies.put(APICONSTANTS.authCookie.user_id, response.member_id);
 
-                            $state.go('projects');
-                        },
-                        function(error) {
-                            $state.go('sign-up');
+                    if (response.payment) {
+                        $scope.name = response.payment.name;
+                        $scope.number = response.payment.last4;
+                        $scope.month = response.payment.exp_month;
+                        $scope.year = response.payment.exp_year;
+                        $scope.cvc = '***';
+                        $scope.zip = response.payment.address_zip;
+
+                        if (response.payment.last4) {
+                            $scope.premiumClicked = true;
                         }
-                    );
+                    }
                 },
                 function(error) {
 
+                    $scope.name = 'card name';
+                    $scope.number = 'card number';
+                    $scope.month = 'exp month';
+                    $scope.year = 'exp year';
+                    $scope.cvc = 'cvc';
+                    $scope.zip = 'zip code';
+                }
+            );
+        }
+
+        $scope.annualPlan = function() {
+            $scope.annualChecked = true;
+            $scope.monthlyChecked = false;
+        };
+
+        $scope.monthlyPlan = function() {
+            $scope.annualChecked = false;
+            $scope.monthlyChecked = true;
+        };
+
+        $scope.updateUser = function(data) {
+            var paymentData;
+
+            paymentData = {
+                plan_name: data.paymentType ? 'annual' : 'monthly',
+                card: {
+                    name: data.cardName,
+                    number: data.cardNumber,
+                    exp_month: data.month,
+                    exp_year: data.year,
+                    cvc: data.cvc,
+                    address_zip: data.zip
+                }
+            };
+
+            UsersResource.user().update({id: userId, payment: paymentData}).$promise.then(
+                function(response) {
+                    $scope.$parent.close();
+                },
+                function(error) {
                 }
             );
         };
@@ -68,5 +101,7 @@ angular.module('ua5App')
             var data = $scope.model;
             $scope.updateUser(data);
         });
+
+        initialValues();
     }])
 ;
