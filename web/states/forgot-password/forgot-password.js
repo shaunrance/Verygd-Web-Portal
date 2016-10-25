ea255e184dc249cc4072de65b7e9e05c72b2f08c
Greg Carr <gregcarrart@gmail.com>
2016-10-24 18:14:40 -0700

    forgot password - set up forgot password page front end

---
+++ web/states/forgot-password/forgot-password.js
@@ -0,0 +1,42 @@
+/* global angular */
+angular.module('ua5App.forgot-password')
+    .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+        $stateProvider.state('forgot-password', {
+            url: '/forgot-password',
+            templateUrl: 'states/forgot-password/forgot-password.html',
+            controller: 'ForgotPasswordCtrl',
+            controllerAs: 'ctrl',
+            resolve: {
+                user: ['APICONSTANTS', '$cookies', function(APICONSTANTS, $cookies) {
+                    var userId = $cookies.get(APICONSTANTS.authCookie.user_id);
+                    var token = $cookies.get(APICONSTANTS.authCookie.user_id);
+
+                    if (userId && token) {
+                        return true;
+                    } else {
+                        return false;
+                    }
+                }]
+            }
+        });
+    }])
+    .controller('ForgotPasswordCtrl', ['$scope', 'user', 'AuthResource', 'UsersResource', '$state', 'APICONSTANTS', '$cookies', 'ModalService', '$rootScope', '$http', function($scope, user, AuthResource, UsersResource, $state, APICONSTANTS, $cookies, ModalService, $rootScope, $http) {
+
+        $scope.submit = function(data) {
+            if (!data && (!data.email)) {
+                return false;
+            }
+
+            AuthResource.token().retrieve({username: data.email}).$promise.then(
+                function(response) {
+                    //send email code
+                    console.log(response);
+                },
+                function(error) {
+                    console.log(error);
+                    //$scope.loginError = true;
+                }
+            );
+        };
+    }])
+;
