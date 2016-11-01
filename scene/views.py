b89248b4ec7649bc9ec6d99dbfabd6b613c75bf1
Andrew <andrew@useallfive.com>
2016-10-31 18:54:30 -0700

    Add request setup to scene views.

--- scene/views.py
+++ scene/views.py
@@ -1,9 +1,10 @@
 from rest_framework import viewsets
 from scene.models import Scene
 from scene.serializers import SceneSerializer
+from very_gd.views import RequestSetup
 
 
-class SceneViewSet(viewsets.ModelViewSet):
+class SceneViewSet(viewsets.ModelViewSet, RequestSetup):
     model = Scene
     serializer_class = SceneSerializer
 
