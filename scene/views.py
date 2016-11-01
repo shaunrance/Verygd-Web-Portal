92c0ffe7223f3bb9e95102d38c645537c6d3a306
Andrew <andrew@useallfive.com>
2016-10-31 18:56:44 -0700

    Missing scene permissions.

--- scene/views.py
+++ scene/views.py
@@ -2,11 +2,14 @@ from rest_framework import viewsets
 from scene.models import Scene
 from scene.serializers import SceneSerializer
 from very_gd.views import RequestSetup
+from rest_framework.permissions import IsAuthenticated
+from scene.permissions import ScenePermissions
 
 
 class SceneViewSet(viewsets.ModelViewSet, RequestSetup):
     model = Scene
     serializer_class = SceneSerializer
+    permission_classes = (IsAuthenticated, ScenePermissions, )
 
     def get_queryset(self):
         return Scene.objects.all()
