db2dacc791c6e592eb36328f42fc9f6fe7b64b9a
Andrew <andrew@useallfive.com>
2017-01-09 17:38:24 -0800

    Refactor custom user model.  Add hotspots.  #156.

--- panel/tests.py
+++ panel/tests.py
@@ -1,3 +1,25 @@
-from django.test import TestCase
+from very_gd.tests.base import TestAPIBase
+from project.tests import TestProject
 
-# Create your tests here.
+
+class TestPanel(TestAPIBase):
+    def __init__(self, *args, **kwargs):
+        super(TestPanel, self).__init__(*args, **kwargs)
+
+        self.project = TestProject()
+        self.endpoint = 'panel'
+
+        self.scene_id = None
+        self.member = None
+        self.project_id = None
+
+    def setUp(self):
+        super(TestPanel, self).setUp()
+
+        self.member = self.users.new_user()
+        self.project_id = self.project.add_new_project(self.member)
+        self.scene_id = self.add_scene(self.member, project=self.project_id)
+
+    def test_scene_hotspots(self):
+        pass
+
