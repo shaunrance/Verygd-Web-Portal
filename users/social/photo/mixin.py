3b90974ec6589c5ff42c05cc16333a08a0062e06
Andrew <andrew@useallfive.com>
2017-01-31 11:32:10 -0800

    Add google profile pic support.  #157.

--- users/social/photo/mixin.py
+++ users/social/photo/mixin.py
@@ -1,4 +1,5 @@
 import shortuuid
+import json
 from io import BytesIO
 
 from django.db import models
@@ -13,8 +14,18 @@ class SocialPhotoMixin(models.Model):
     class Meta:
         abstract = True
 
+    def _update_photo_from_content(self, content):
+        photo = ContentFile(content)
+        img = Image.open(BytesIO(content))
+
+        file_ext = {v: k for k, v in Image.EXTENSION.items()}[img.format]
+
+        photo.name = '{0}_{1}_social_photo{2}'.format(shortuuid.uuid(), self.user.username.lower(), file_ext)
+
+        self.update_photo(photo)
+
     def update_photo_from_facebook(self, *args, **kwargs):
-        url = 'http://graph.facebook.com/{0}/picture?type=large'.format(self.user.social_user.uid)
+        url = 'http://graph.facebook.com/{0}/picture'.format(self.user.social_user.uid)
 
         try:
             response = request('GET', url, params={'type': 'large'})
@@ -22,17 +33,27 @@ class SocialPhotoMixin(models.Model):
         except HTTPError:
             pass
         else:
-            photo = ContentFile(response.content)
-            img = Image.open(BytesIO(response.content))
-
-            file_ext = {v: k for k, v in Image.EXTENSION.items()}[img.format]
-
-            photo.name = '{0}_{1}_social_photo{2}'.format(shortuuid.uuid(), self.user.username.lower(), file_ext)
-
-            self.update_photo(photo)
+            self._update_photo_from_content(response.content)
 
     def update_photo_from_google(self, *args, **kwargs):
-        pass
+        url = 'https://www.googleapis.com/oauth2/v1/userinfo?alt=json&access_token={0}'.format(
+            self.user.social_user.access_token)
+
+        try:
+            response = request('GET', url)
+            response.raise_for_status()
+        except HTTPError:
+            pass
+        else:
+            user_meta = json.loads(response.content.decode('unicode-escape'))
+
+            try:
+                response = request('GET', user_meta['picture'])
+                response.raise_for_status()
+            except HTTPError:
+                pass
+            else:
+                self._update_photo_from_content(response.content)
 
     def update_social_photo(self):
         provider = self.user.social_user.provider
