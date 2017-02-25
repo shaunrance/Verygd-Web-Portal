bc0f710a38b6930418ab7b1fb49f1d51607574ed
Andrew <andrew@useallfive.com>
2017-02-24 16:58:11 -0800

    Fix case where a payment may exist but not have a plan_name.

--- users/models.py
+++ users/models.py
@@ -67,7 +67,8 @@ class Member(VeryGDMember):
 
     @property
     def subscription_type(self):
-        if self.payment and self.payment.plan_name in ('beta_yearly', 'beta_monthly'):
+        if self.payment and hasattr(self.payment, 'plan_name') and self.payment.plan_name in ('beta_yearly',
+                                                                                              'beta_monthly'):
             return 'paid'
         elif self.has_premium_account:
             return 'granted'
