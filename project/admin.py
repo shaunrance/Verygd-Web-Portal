5a8f4edae5717cbfc229a40a54d2d16f54edc4da
Andrew Silvernail <andrew@useallfive.com>
2017-03-21 18:42:20 -0700

    Add `featured` and `featured_order` fields.  Add projects to admin.
    This is the first iteration for adding featured projects.

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,20 @@
-from django.contrib import admin
+from __future__ import unicode_literals
 
-# Register your models here.
+from django.contrib import admin
+from project.models import Project
+from media_portal.admin import admin_site
+
+
+@admin.register(Project, site=admin_site)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'owner', 'short_link', 'public', 'first_image', 'featured', )
+
+    def short_link(self, instance):
+        return 'https://{site_url}/public/project/{short_uuid}'.format(
+            site_url='api.very.gd',
+            short_uuid=instance.short_uuid)
+
+    def first_image(self, instance):
+        first_image = next(instance.content)
+
+        return '{first_image}'.format(first_image=first_image)
