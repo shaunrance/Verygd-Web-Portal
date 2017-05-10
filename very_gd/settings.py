da6bd3255b57fe7c0d5f3c8a5d9fb4c481ea049d
Andrew Silvernail <andrew@useallfive.com>
2017-05-10 13:04:33 -0700

    Switches database to mysql.  Adds migration for full_name.

--- very_gd/settings.py
+++ very_gd/settings.py
@@ -175,9 +175,15 @@ TEMPLATES = [
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #    'ENGINE': 'django.db.backends.sqlite3',
+    #    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # },
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+       'ENGINE': 'django.db.backends.mysql',
+       'OPTIONS': {
+           'read_default_file': '/etc/mysql.cnf',
+       },
     }
 }
 
