00cf8395ce4219a7d9dca83037486b389a59d55f
Andrew Silvernail <andrew@useallfive.com>
2017-05-30 18:04:12 -0700

    This adds a custom auth class + add'l permissions to private projects in order to allow only password-protected private projects to be accessible publicly.
    * Added a test to ensure the owner of the project can access it w/o a
    password
    
    * The custom auth class verifies the following before allowing access:
       * That the request is a GET request w/ a password query param
       * That the project id is a valid project id
       * That the project id requested is also password-protected
    
    * Later on the additional perms added to ProjectPermissions check for
    either an auth'ed user or a user with a (valid) password.

---
+++ project/auth.py
@@ -0,0 +1,27 @@
+from project.models import Project
+from rest_framework.authentication import BaseAuthentication
+from django.contrib.auth.models import AnonymousUser
+from rest_framework import exceptions
+from django.utils.translation import ugettext_lazy as _
+
+
+class PrivateProjectAuthentication(BaseAuthentication):
+    def authenticate(self, request):
+        if 'password' in request.query_params and request.method == 'GET':
+            project = project_pk = None
+            path_parts = request.path.split('/')[1:]
+
+            if path_parts[0] == 'project':
+                if isinstance(path_parts[1], int) and path_parts[1] > 0:
+                    project_pk = path_parts[1]
+
+            if project_pk and Project.objects.filter(pk=project_pk, password__isnull=False).count():
+                project = Project.objects.get(pk=project_pk)
+
+            if project:
+                anonymous_user = AnonymousUser()
+                return anonymous_user, None
+
+            raise exceptions.AuthenticationFailed(_('not a valid project id.'))
+        else:
+            return None
--- project/tests.py
+++ project/tests.py
@@ -71,6 +71,10 @@ class TestProject(TestAPIBase):
 
         self.assertEquals(response.status_code, 403)
 
+        # project owner can access without a password
+        response, msg = self.get_as(self.member, detail_url)
+        self.assertEquals(response.status_code, 200)
+
         response, msg = self.get_as(self.anonymous_member, ''.join([detail_url, '?password=test']))
 
         self.assertEquals(response.status_code, 200)
@@ -105,9 +109,10 @@ class TestProject(TestAPIBase):
 
         self.assertEquals(response.status_code, 200)
 
+        # project is now private again
         response, msg = self.get_as(self.anonymous_member, detail_url)
 
-        self.assertEquals(response.status_code, 200)
+        self.assertEquals(response.status_code, 403)
 
     def test_num_of_private_projects(self):
         self.project_id = self.add_new_project(self.member, public=False)
