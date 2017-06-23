91aaa58b1f52966324812e069e01d87b816bb673
Jason Farrell <jason@useallfive.com>
2017-06-22 18:08:54 -0700

    Add e2e test to deploy command.

--- Gruntfile.js
+++ Gruntfile.js
@@ -230,7 +230,7 @@ module.exports = function(grunt) {
         protractor: {
             options: {
                 configFile: 'test/protractor-conf.js', // Default config file
-                keepAlive: true, // If false, the grunt process stops when the test fails.
+                keepAlive: false, // If false, the grunt process stops when the test fails.
                 noColor: false, // If true, protractor will not use colors in its output.
                 args: {
                     // Arguments passed to the command
@@ -322,6 +322,7 @@ module.exports = function(grunt) {
     grunt.registerTask('stage', stageTasks);
 
     prodTasks = [
+        'protractor',
         'ngtemplates:app',
         'concat:scss',
         'sass',
